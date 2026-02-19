3678759035a181c8ac07961285420304bcadf75d
NRD <ndalbah2@gmail.com>
2026-02-18 23:02:15 -0500

    fixed firebase

--- TomeMate/TomeMateApp.swift
+++ TomeMate/TomeMateApp.swift
@@ -7,10 +7,23 @@
 
 import SwiftUI
 import CoreData
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct TomeMateApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @StateObject var authManager = AuthManager()
     let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         let context = persistenceController.container.viewContext
         let dateHolder = DateHolder(context)
@@ -18,6 +31,7 @@ struct TomeMateApp: App {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
                 .environmentObject(dateHolder)
+                .environmentObject(authManager)
         }
     }
 }
