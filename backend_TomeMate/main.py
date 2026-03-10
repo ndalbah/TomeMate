fad3205a23d7779412985ef97c3c93c72fa03253
Derrick Mangari <dmangari@live.fr>
2026-03-10 15:59:57 -0400

    fixed creatures api

--- backend_TomeMate/main.py
+++ backend_TomeMate/main.py
@@ -184,13 +184,13 @@ def get_creatures(
     results = mapped_bestiary
 
     if name is not None:
-        results = [b for b in results if b["name"] == name]
+        results = [b for b in results if b and b.get("name") == name]
 
     if type is not None:
-        results = [b for b in results if b ["type"] == type]
+        results = [b for b in results if b and b.get("type") == type]
 
     if subtype is not None:
-        results = [b for b in results if b ["subtype"] == subtype]
+        results = [b for b in results if b and b.get("subtype") == subtype]
 
     total = len(results)
     start = (page - 1) * page_size
@@ -208,11 +208,6 @@ def get_creature_by_id(creature_id: str):
             return creature
     raise HTTPException(status_code=404, detail=f"Creature with id '{creature_id}' not found")
 
-
-@app.get("/creatures")
-def get_creatures():
-    results = mapped_bestiary
-    return results
 
 
 ###### Items ###########
