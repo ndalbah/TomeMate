453bbe6e0f01d3c9a1e9b736a59828650dfd1a24
Justin Pescador <j.pescador03@gmail.com>
2026-03-31 22:02:03 -0400

    Added AddItem and AddSpell from JSON data + organized files in respective folders

---
+++ TomeMate/Views/Add/AddItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemView.swift
+//  TomeMate
+//
+//  Created by Justin Pescador on 2026-03-31.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddItemView()
+}
---
+++ TomeMate/Views/Add/AddSpellView.swift
@@ -0,0 +1,18 @@
+//
+//  AddSpellView.swift
+//  TomeMate
+//
+//  Created by Justin Pescador on 2026-03-31.
+//
+
+import SwiftUI
+
+struct AddSpellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddSpellView()
+}
